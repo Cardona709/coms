6e33cba4a390187d65f14ee46fbeab58eb799e3b
Cardona709 <145276741+Cardona709@users.noreply.github.com>
2023-12-02 15:40:39 +0000

    Add files via upload

--- mainwindow.h
+++ mainwindow.h
@@ -3,7 +3,8 @@
 
 #include <QMainWindow>
 #include <QStyle>
-#include "MyTCPServer.h"
+#include <QHostAddress>
+#include "DeviceController.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -18,19 +19,24 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_btnStartServer_clicked();
+    void on_lnIPAddress_textChanged(const QString &arg1);
 
-    void newClinetConnected();
+    void on_btnConnect_clicked();
 
-    void on_btnSendToAll_clicked();
+    void device_connected();
+    void device_disconnected();
+    void device_stateChanged(QAbstractSocket::SocketState);
+    void device_errorOccurred(QAbstractSocket::SocketError);
+    void device_dataReady(QByteArray data);
 
-    void clientDisconnected();
 
-    void clientDataReceived(QString message);
-    void writeown(QString message, QString name);
+    void on_btnSend_clicked();
 
 private:
     Ui::MainWindow *ui;
-    MyTCPServer *_server;
+    DeviceController _controller;
+
+    //methods
+    void setDeviceContoller();
 };
 #endif // MAINWINDOW_H
